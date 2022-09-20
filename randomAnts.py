3340236e9bc092d64502f09bcd1b1782349bcffc
Ian <44474914+trapperian@users.noreply.github.com>
2022-09-20 15:42:25 -0600

    Initial commit

---
+++ randomAnts.py
@@ -0,0 +1,8 @@
+class Ants:
+
+class Graph(dict):
+    def __init__(self, vs=[], es=[]): # vs is list of vertices and es is list of edges
+        for v in vs:  # I think that this is to add vertices.
+            self.add_vertex(v)
+
+
