b409b75beb55950d7e860521f3dae30dc4ee2330
Ian <44474914+trapperian@users.noreply.github.com>
2022-09-21 06:49:01 -0600

    Create snowflakes.py
    Following https://medium.com/nerd-for-tech/programming-fractals-in-python-d42db4e2ed33 to build koch snowflake.
    Having issues of triangles not being placed midway through the equilateral triangles build previously

---
+++ snowflakes.py
@@ -0,0 +1,29 @@
+# following tutorial from: https://medium.com/nerd-for-tech/programming-fractals-in-python-d42db4e2ed33
+
+import turtle
+
+def koch_fract(turtle, divis, size):
+    if(divis == 0):
+        turtle.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_fract(turtle, divis - 1, size / 3)
+            turtle.forward(1)
+            turtle.left(angle)
+
+
+# variables for koch_fract function
+divis = 10
+size = 2000
+
+
+# setting up turtle
+wn = turtle.Screen()
+wn.setup()
+turtle.speed(500)
+turtle.pendown()
+
+
+for i in range(0, 3):
+   koch_fract(turtle, divis, size)
+   turtle.left(-120)
