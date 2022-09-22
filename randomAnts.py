1a173b149b69796fc7c6f26b6a6fa1f177c55af6
Ian <44474914+trapperian@users.noreply.github.com>
2022-09-21 19:03:27 -0600

    Adding classes to randomAnts

--- randomAnts.py
+++ randomAnts.py
@@ -1,8 +1,17 @@
-class Ants:
+import random
+import turtle
 
-class Graph(dict):
-    def __init__(self, vs=[], es=[]): # vs is list of vertices and es is list of edges
+class Ants(turtle):
+    def __init__(self):
+
+#   is this one needed?
+    class Graph(dict):
+#   vs is list of vertices and es is list of edges
+    def __init__(self, vs=[], es=[]):
         for v in vs:  # I think that this is to add vertices.
             self.add_vertex(v)
 
+
+class Food():
+    def __init__(self):
 
--- snowflakes.py
+++ snowflakes.py
@@ -8,13 +8,13 @@ def koch_fract(turtle, divis, size):
     else:
         for angle in [60, -120, 60, 0]:
             koch_fract(turtle, divis - 1, size / 3)
-            turtle.forward(1)
+            turtle.forward(2)
             turtle.left(angle)
 
 
 # variables for koch_fract function
 divis = 10
-size = 2000
+size = 1
 
 
 # setting up turtle
@@ -26,4 +26,3 @@ turtle.pendown()
 
 for i in range(0, 3):
    koch_fract(turtle, divis, size)
-   turtle.left(-120)
